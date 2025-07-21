9691e5e5ae3cdc5fdb4a2b4c11db0dae788574d4
Alaoscorty <133812869+Alaoscorty@users.noreply.github.com>
2025-07-21 02:07:30 +0100

    new

--- scripts.js
+++ scripts.js
@@ -2,24 +2,25 @@ document
   .getElementById("reservationForm")
   .addEventListener("submit", function (e) {
     e.preventDefault();
+
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
     const phone = document.getElementById("phone").value.trim();
     const service = document.getElementById("service").value.trim();
     const date = document.getElementById("date").value.trim();
     const message = document.getElementById("message").value.trim();
-    const projet = document.getElementById("projet").value.trim();
+    const projet = document.getElementById("urgent").checked ? "Oui" : "Non";
 
     let text = `Bonjour, je souhaite réserver un service :%0A`;
-    text += `Nom : ${name}%0A`;
-    text += `Adresse email : ${email}%0A`;
-    text += `Téléphone : ${phone}%0A`;
-    text += `Service : ${service}%0A`;
-    text += `Date souhaitée : ${date}%0A`;
-    text += `Nmessage : ${message}%0A`;
-    text += `projet : ${projet}%0A`;
-    if (message) text += `Message : ${message}%0A`;
+    text += `👤 Nom : ${name}%0A`;
+    text += `📧 Email : ${email}%0A`;
+    if (phone) text += `📞 Téléphone : ${phone}%0A`;
+    text += `🛠️ Service : ${service}%0A`;
+    text += `📅 Date souhaitée : ${date}%0A`;
+    text += `⚡ Projet urgent : ${projet}%0A`;
+    text += `💬 Message : ${message}%0A`;
+
     const whatsappNumber = "22951563219";
-    const url = `https://wa.me/${whatsappNumber}?text=${text}`;
+    const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
     window.open(url, "_blank");
   });
